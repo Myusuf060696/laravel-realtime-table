1482d5689227288ca25f78189c6413a09edc900b
myusuf <muhammadyusuf931@gmail.com>
2018-05-27 04:14:18 +0700

    first

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Card;
+use App\User;
+use App\Events\ScoreUpdated;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    public function show(Card $card) {
+        $user = auth()->user();
+        $user->score = $user->score + $card->value;
+        $user->save();
+        event(new ScoreUpdated($user)); // broadcast `ScoreUpdated` event
+        return redirect()->back()->withValue($card->value);
+    }
+
+    public function leaderboard () {
+        return User::all(['id', 'name', 'score']);
+    }
+}
